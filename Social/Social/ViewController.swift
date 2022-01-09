a032fbe516025287a8c8c59f92219a44726c2c29
bmarkowitz <batting1000@comcast.net>
2022-01-09 10:24:01 -0500

    Added app icons and removed storyboard

---
+++ Social/Social/ViewController.swift
@@ -0,0 +1,16 @@
+//
+// ViewController.swift
+// Social
+//
+// Created by Brett Markowitz on 1/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
